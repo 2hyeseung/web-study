3a14bcb880a799d20eb54cf65c35205634cc9249
2hyeseung <leehs226@naver.com>
2022-11-01 09:21:25 +0900

    study: form 실습 예제

--- 12_form/app.js
+++ 12_form/app.js
@@ -18,6 +18,7 @@ app.get('/',function(req, res){
     // GET / (http://localhost:PORT)
     res.render('index', {title:"폼 실습을 해보자", desc:"여기는 설명"}); // views/index.ejs 파일을 찾아서 응답
 })  
+
 app.get('/getForm',function(req,res){
     // GET 요청은 req.query 객체에 폼 정보가 전달
     console.log(req.query);
@@ -36,6 +37,32 @@ app.post('/postForm',function(req,res){
         userInfo : req.body,
     })
 })
+
+app.get('/exercise26', function (req, res) {
+    res.render('exercise26');
+  });
+  app.get('/getForm26',function(req,res){
+    // GET 요청은 req.query 객체에 폼 정보가 전달
+    console.log(req.query);
+    // res.send('get 요청 응답 성공!');
+    res.render('result26',{
+        title:'GET 요청 성공',
+        userInfo : req.query,
+    });
+})
+app.get('/exercise27', function (req, res) {
+    res.render('exercise27');
+});
+app.post('/postForm27',function(req,res){
+    // POST 요청은 req.body 객체에 폼 정보가 전달
+    console.log(req.body);
+    // res.send('post 요청 응답 성공!');
+    res.render('result27',{
+        title:'POST 요청 성공',
+        userInfo : req.body,
+    })
+})
+
 
 app.listen(PORT,function(){
     console.log(`http://localhost:${PORT}`);
